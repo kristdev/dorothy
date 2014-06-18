eeb5192ee963ba094067063c9e1126c949635c4e
kristdev <pambeaba@yahoo.fr>
2014-06-18 16:53:45 +0100

    180614-1653
    Controller Home

--- application/modules/home/controllers/copythis.php
+++ application/modules/home/controllers/copythis.php
@@ -1,12 +1,16 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Copythis extends MX_Controller {
+class Home extends MX_Controller {
 
        public function __construct()
        {
             parent::__construct();
             // Your own constructor code
+       }
+
+       public function index(){
+       		echo 'welcome ';
        }
 }
 ?>
