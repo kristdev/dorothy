95c179907988a1ed6ad99c16fd8ac352ca877cdc
kristdev <pambeaba@yahoo.fr>
2014-06-19 00:44:00 +0100

    190614-0043
    font awesome added

--- application/modules/home/views/homeview.php
+++ application/modules/home/views/homeview.php
@@ -2,13 +2,22 @@
 <html lang="fr">
 <head>
 	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+
 	<title>Dorothy - Cameroon University Search engine</title>
 
 	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bootstrap/dist/css/bootstrap.min.css">
-
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/stylesheet.css">
 </head>
 <body>
 	
+	
+
 	<!-- Coeur jquery -->
 	<script src="<?php echo base_url(); ?>assets/jquery/dist/jquery.min.js"></script>
 
