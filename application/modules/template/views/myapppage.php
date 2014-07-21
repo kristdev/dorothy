5039c876493f116c4ab34e785bd5cf5d7b48e38b
kristdev <pambeaba@yahoo.fr>
2014-07-21 10:20:36 +0100

    210714-1020
    updated

--- application/modules/template/views/myapppage.php
+++ application/modules/template/views/myapppage.php
@@ -68,10 +68,30 @@
 
 <!-- Add fancyBox main JS and CSS files -->
 <script src="<?php echo base_url(); ?>assets/js/global.js"></script>
+
 <script>
 $(document).ready(function() {
-    $.navigate.init();
-});
+  //**** OPTIONAL ****
+    // Example of a custom ajax insert function
+    (function($) {
+      $.fn.fadeInsert = function(options) {
+        console.log(options);
+        //the option is an array : {html:the ajax html, scripts: the scripts that already are in the html, customData:any data you associated to this state during navigate} 
+        var that = $(this);
+        that.fadeOut(300, function(){
+          that.html(options.html);
+          that.fadeIn(300, function(){
+            that.trigger({type:"finishrefreshinsert"});
+          });
+        });
+        return this;
+      };
+    })(jQuery); 
+  
+  //THIS IS THE ONLY NEEDED LINE
+  //Just init navigate and any link without target="_blank" will become an ajax link
+  $.navigate.init();
+});  
 </script>
 
 </body>
